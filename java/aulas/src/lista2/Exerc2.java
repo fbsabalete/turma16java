b8a4b0e838aeacea3a712c9d3993b879ce4da45b
Fernando Sabalete <fb.sabalete@gmail.com>
2021-01-27 16:40:17 -0300

    Correção do exercício 2

--- java/aulas/src/lista2/Exerc2.java
+++ java/aulas/src/lista2/Exerc2.java
@@ -6,43 +6,26 @@ public class Exerc2 {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		
-		int num1, num2, num3, primeiro, segundo, terceiro;
+		int numeros[] = new int[3]; 
+		int tempNum;
 		
-		System.out.print("Digite o primeiro numero: ");
-		num1 = read.nextInt();
-		System.out.print("Digite o segundo numero: ");
-		num2 = read.nextInt();
-		System.out.print("Digite o terceiro numero: ");
-		num3 = read.nextInt();
-		read.close();
-		if(num1 < num2 && num1 < num3) {
-			primeiro = num1;
-			if(num2 < num3) {
-				segundo = num2;
-				terceiro = num3;
-			}else {
-				segundo = num3;
-				terceiro = num2;
-			}
-		}else if(num2 < num1 && num2 < num3) {
-			primeiro = num2;
-			if(num1 < num3) {
-				segundo = num1;
-				terceiro = num3;
-			}else {
-				segundo = num3;
-				terceiro = num1;
-			}
-		}else {
-			primeiro = num3;
-			if(num2 < num1) {
-				segundo = num2;
-				terceiro = num1;
-			}else {
-				segundo = num1;
-				terceiro = num2;
+		for(int i = 0; i < 3; i++) {
+			System.out.println("Digite um numero: ");
+			numeros[i] = read.nextInt();
+		}
+		
+		for(int i = 0; i < 3; i++) {
+			for(int j = i + 1; j < 3; j++) {
+				if(numeros[i] > numeros[j]) {
+					tempNum = numeros[i];
+					numeros[i] = numeros[j];
+					numeros[j] = tempNum;
+				}
 			}
 		}
-		System.out.printf("1º -- %d \n2º -- %d \n3º -- %d", primeiro, segundo, terceiro);
+		
+		for(int i = 0; i < 3; i++) {
+			System.out.printf("%d ", numeros[i]);
+		}
 	}
 }
